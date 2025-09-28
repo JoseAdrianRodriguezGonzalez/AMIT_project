704f2727670520ddb8fe7144e347da7bd3c1adf0
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2025-09-28 00:26:43 -0600

    Documentacion de la clase data_loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import os
 class data_loader:
-    def __init__(self,files):
+    """Esta clase consiste en poder cargas diferentes tipos de archivos.
+        Como atributos tiene una lista de nombers de archivos que puedan estar contenidos dentro de una carpeta
+        Posee los método de cargar archivo
+    """
+    def __init__(self,files:str|iter[str]):
+        """_summary_
+        Método constructor que almacena el nombre o los nombres de los archvios
+        Args:
+            files (lista o string): Nombre o nombres de archivos
+
+        Raises:
+            ValueError: Erro en dado caso de que el archivo se encontró que no tiene una extensión
+            FileNotFoundError: Erro en dado caso que el archivo con determinado nombre no exista
+        """
         if isinstance(files,str):
             files=[files]
         self.files=files
@@ -11,6 +24,17 @@ class data_loader:
             if not os.path.exists(name_file):
                 raise FileNotFoundError(f"El archivo {name_file} no existe")
     def load(self):
+        """_summary_
+        Carga los datos del (los) archivo(s)en forma de diccionario.
+        Las keys son el nombre del archivo y el value es su contenido 
+        Raises:
+            ValueError: Si el formato que recupero no está dentro de los válidos
+            RuntimeError: Si al cargar los datos de los elementos encontrados no se encuentre completo o tenga
+            errores de lectura
+
+        Returns:
+            dict: Diccionario que contiene los datasets.
+        """
         datasets={}
         for f in self.files:
             try:
