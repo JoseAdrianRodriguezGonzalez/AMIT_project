966cc42d5161f577aeb6de086ab8d4b7961cdeb7
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2025-09-28 16:53:26 -0600

    añadiendo la documentación

--- src/llm_summary.py
+++ src/llm_summary.py
@@ -1,7 +1,18 @@
 from llama_cpp import Llama
 import re
 class LlamaModel:
-    def __init__(self,model):
+    """Clase que posee la iteracción con un modelo de LLM. Este modelo puede ser llama u otro tipo de modelo de GGUF
+    model_name=nombre del modelo
+    model=instancia de llama que inicia el modelo
+    freq: las palabras que se usarán para dar un inferencia
+    generetaed_titles : titutlos generados 
+    generated_interpretations: Interpretaciones generadas 
+    """
+    def __init__(self,model:str):
+        """Método constructor que inicializa todos los atributos
+        Args:
+            model (str): Nombre del modelo
+        """
         self.model_name=model
         self.model=Llama(model_path=model,
                              n_ctx=4096,          
@@ -12,7 +23,17 @@ class LlamaModel:
         self.generated_titles = None
         self.generated_interpretations = None
 
-    def request_title(self,freq,params=None):
+    def request_title(self,freq:list[list[str]],params:dict=None)->list[str]:
+        """Se crea/genera un titulo dado a una lista de tópics
+
+        Args:
+            freq (list[list[str]]): lista de tópicos de palabras que provienen de tópicos extraídos por berTOPIC
+            params (dict, optional): Diccionario de parámetros que se le pueden pasar al modelo
+            con el fin de mejorar o cambiar la inferencia que resulta. Defaults to None.
+
+        Returns:
+            list[str]: lista de titulos que salieron por el conjunto de tópicos
+        """        
         if params==None:
             params = {
                 "max_tokens": 100,
@@ -44,7 +65,16 @@ class LlamaModel:
             titles.append(out)
         self.generated_titles=titles
         return titles
-    def request_interpretation(self,freq,params=None):
+    def request_interpretation(self,freq:list[list[str]],params:dict=None)->list[str]:
+        """En base a un conjunto de palabras de tópicos, genera interpretaciones por tópicos
+        Args:
+            freq (list[list[str]]):Se tiene la lista de tópicos y dentro las palabras de donde proviene
+            params (dict, optional): Diccionario de parámetros para modificar el como funciona el 
+            modelo de lenguaje. Defaults to None.
+
+        Returns:
+            list[str]: Conjunto de descripciones por tópico
+        """        
         if params==None:
             params = {
                 "max_tokens": 400,
@@ -75,8 +105,18 @@ class LlamaModel:
             titles.append(out)
         self.generated_interpretations=titles
         return titles
-    def get_titles(self):
+    def get_titles(self)->list[str]:
+        """función que obtiene el conjunto de titulos
+
+        Returns:
+            list[str]: conjunto de strings con los títulos generados
+        """        
         return self.generated_titles
 
-    def get_interpretations(self):
+    def get_interpretations(self)->list[str]:
+        """Obtiene las interpretaciones almacenadas
+
+        Returns:
+            list[str]: conjunto de strings con las interpretaciones por tópico
+        """
         return self.generated_interpretations
