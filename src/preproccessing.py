39167c6ce3aa836f9040c727a7fc47531a490ac5
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2025-09-28 00:50:33 -0600

    añadir la documentación de vectorizer y de preprocessing

--- src/preproccessing.py
+++ src/preproccessing.py
@@ -1,10 +1,31 @@
 import nltk
 from nltk.corpus import stopwords
 class Preprocessor:
+    """Preprocessor
+    Clase que hace un preprocesamiento ligero de texto
+    Posee el atributo stopword, que indica la lista de stopswords en un idioma determinado
+    Posee su método constructor y una función que formatea al conjunto de datos
+    """
     def __init__(self,language="spanish"):
+        """Método constructor que inicializa el atributo de stepwords
+        Args:
+            language (str, optional): Cadena que indica en que idioma se necesitan los stepwords
+            . Defaults to "spanish".
+        """
         nltk.download("stopwords",quiet=True)
         self.stopword = stopwords.words(language)
     def transform(self,df):
+        """Función que se dedica a transformar las columnas de los comentarios si es necesario
+
+        Args:
+            df (pd.Dataframe): Es un conjunto de datos de pandas
+
+        Raises:
+            ValueError: Si el formato no es como el esperado
+
+        Returns:
+            lista: Los comentarios en forma de lista 
+        """
         if "Titulo" in df.columns and "Review" in df.columns:
             docs=(
                 df["Titulo"].fillna('') + " " +
--- src/vectorizer.py
+++ src/vectorizer.py
@@ -1,7 +1,24 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 class Vectorizer:
-    def __init__(self, stopwords, max_df=0.3):
+    """
+    Clase que vectoriza las palabras previamente filtradas en el proceso de preprocessing.
+    Se tienen los atributos de stepwords, para identificar aquellas palabras que son muy frecuentes por tópico.
+    max_df, que es la máxima tolerancia aceptada para tolerar que es o no un stepword adicional
+    global_vectorizer, donde se guarda el vector resultante
+    filtered_vocab, es el corpus formado después de vectorizar y filtrar palabras
+    topic_vectorizer, segundo proceso de filtrado 
+    Posee el método constructor, el método de build vocab, que permite construir el vocabulario y la segunda
+    revisada de vectorización
+    """
+    def __init__(self, stopwords:list[str], max_df=0.3):
+        """Método constructor que permite inicializar cada una de los atributos
+
+        Args:
+            stopwords (list): Es una lista de strings que tiene los stepwords
+            max_df (float, optional): Cantidad máxima de detección de stepword o palabra demasiado frecuente
+            bajo el contexto del tópico y que no aporte información relevante. Defaults to 0.3.
+        """
         self.stopwords = stopwords
         self.max_df = max_df
         self.global_vectorizer = None
@@ -9,7 +26,15 @@ class Vectorizer:
         self.topic_vectorizer = None
 
     def build_vocab(self, docs):
-        """Primera pasada: crea vocabulario filtrado"""
+        """Primera pasada: crea vocabulario filtrado através de los step words de NLTK
+        Args:
+            docs (pd.Dataframe): tópicos después de un acómodo de datos por parte del conjunto de datos 
+
+        Returns:
+            tupla de sparse matrix y numpy array: Retorna la matriz generada después de calcular su frecuencia de TFID
+            y el numpy array contiene las palabras encontradas 
+        """
+    
         self.global_vectorizer = TfidfVectorizer(
             stop_words=self.stopwords,
             max_df=self.max_df
@@ -19,7 +44,12 @@ class Vectorizer:
         return X, self.filtered_vocab
 
     def get_topic_vectorizer(self):
-        """Crea vectorizer definitivo con vocab filtrado"""
+        """Crea vectorizer definitivo con vocab filtrado si en dado caso existen palabras irrelevantes
+        Raises:
+            ValueError:En caso de no haber ejecutado el primer proceso de vectorización
+        Returns:
+            matriz de tfid: matriz de tfid caracteristicas
+        """
         if self.filtered_vocab is None:
             raise ValueError("Debes ejecutar build_vocab primero")
         self.topic_vectorizer = TfidfVectorizer(
